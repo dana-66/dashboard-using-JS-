7f1e7af4ae63cf44afac0930723367c5fe08c57e
dana-66 <danatarik66@gmail.com>
2024-09-09 17:31:34 +0300

    final project for dashboard

---
+++ js/orders.js
@@ -0,0 +1,26 @@
+const orders = [
+    {
+        productName:"BS drone xlsr",
+        productNumber: 3469,
+        paymentStatus:"Due",
+        shipping: "pending"
+    },
+    {
+        productName:"HolySHit drone xlsr",
+        productNumber: 3669,
+        paymentStatus:"paid",
+        shipping: "delivered"
+    },
+    {
+        productName:"GPS drone xlsr",
+        productNumber: 3769,
+        paymentStatus:"paid",
+        shipping: "delivered"
+    },
+    {
+        productName:"When will this be over",
+        productNumber: 3869,
+        paymentStatus:"Refunded",
+        shipping: "Declined"
+    }
+];
